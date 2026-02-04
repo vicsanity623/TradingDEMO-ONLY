437be3e2676e462688f88bf042628f0e4e2186ee
Vic <125237471+vicsanity623@users.noreply.github.com>
2026-02-04 00:10:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'goku-rpg-v4.4'; // UPDATED: Version bump forces cache clear
+const CACHE_NAME = 'goku-rpg-v4.5'; // UPDATED: Version bump forces cache clear
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
